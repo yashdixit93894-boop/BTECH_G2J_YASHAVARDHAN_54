583583fbf24439c039dd9ed8a49f4788739adc64
Yash <yashdixit93894@gmail.com>
2026-02-11 09:50:35 +0530

    Add files via upload

---
+++ 1st.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int sub1, sub2, sub3, sub4,sub5;
+    sub1=88;
+    sub2=80;
+    sub3=90;
+    sub4=73;
+    sub5=80;
+    int sum= sub1+sub2+sub3+sub4+sub5;
+    int avg= sum/5;
+    printf("your avg is %d",avg);
+
+}
